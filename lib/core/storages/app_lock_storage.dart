53660377dd64657d9dc21f174d2241ca0581da50
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-03-02 20:36:56 +0700

    Use old data as initial

--- lib/core/storages/app_lock_storage.dart
+++ lib/core/storages/app_lock_storage.dart
@@ -1,5 +1,6 @@
 import 'package:storypad/core/objects/app_lock_object.dart';
 import 'package:storypad/core/storages/base_object_storages/object_storage.dart';
+import 'package:storypad/core/storages/base_object_storages/bool_storage.dart';
 
 class AppLockStorage extends ObjectStorage<AppLockObject> {
   @override
@@ -11,4 +12,25 @@ class AppLockStorage extends ObjectStorage<AppLockObject> {
   Map<String, dynamic> encode(AppLockObject object) {
     return object.toJson();
   }
+
+  @override
+  Future<AppLockObject?> readObject() async {
+    AppLockObject? data = await super.readObject();
+
+    // TODO: remove this after a while.
+    // ignore: deprecated_member_use_from_same_package
+    bool? deprecatedData = await LocalAuthEnabledStorage().read();
+
+    if (deprecatedData != null) {
+      data = (data ?? AppLockObject.init()).copyWith(enabledBiometric: deprecatedData);
+
+      // ignore: deprecated_member_use_from_same_package
+      LocalAuthEnabledStorage().remove();
+    }
+
+    return data;
+  }
 }
+
+@Deprecated('Will be removed soon.')
+class LocalAuthEnabledStorage extends BoolStorage {}
