f6baa6aa2159637483aa82afcb13e3a5d588d105
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-03-09 14:06:09 +0700

    Add onboarding hello view

--- lib/views/onboarding/onboarding_view_model.dart
+++ lib/views/onboarding/onboarding_view_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:storypad/views/onboarding/steps/step_1/onboarding_step_1_view.dart';
+import 'package:storypad/views/onboarding/steps/onboarding_hello_view.dart';
 import 'package:storypad/widgets/view/base_view_model.dart';
 import 'onboarding_view.dart';
 
@@ -14,7 +14,7 @@ class OnboardingViewModel extends BaseViewModel {
 
   void next(BuildContext context) {
     if (Form.of(context).validate()) {
-      OnboardingStep1Route().push(context);
+      OnboardingHelloRoute().push(context);
     }
   }
 
---
+++ lib/views/onboarding/steps/onboarding_hello_view.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:storypad/views/onboarding/steps/step_1/onboarding_step_1_view.dart';
+import 'package:storypad/widgets/sp_fade_in.dart';
+import 'package:storypad/widgets/view/base_route.dart';
+
+class OnboardingHelloRoute extends BaseRoute {
+  OnboardingHelloRoute();
+
+  @override
+  Widget buildPage(BuildContext context) => OnboardingHelloView(params: this);
+}
+
+class OnboardingHelloView extends StatefulWidget {
+  const OnboardingHelloView({
+    super.key,
+    required this.params,
+  });
+
+  final OnboardingHelloRoute params;
+
+  @override
+  State<OnboardingHelloView> createState() => _OnboardingHelloViewState();
+}
+
+class _OnboardingHelloViewState extends State<OnboardingHelloView> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 2)).then((e) {
+      final context = this.context;
+      if (!context.mounted) return;
+
+      OnboardingStep1Route().pushReplacement(context);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double paddingTop = MediaQuery.of(context).padding.top + 56;
+    double paddingBottom = MediaQuery.of(context).padding.bottom + 24;
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        forceMaterialTransparency: true,
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: EdgeInsets.only(
+          top: paddingTop,
+          bottom: paddingBottom,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "Hello Thea",
+              style: TextTheme.of(context).titleLarge,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              "Let's explore StoryPad together!",
+              style: TextTheme.of(context).bodyLarge,
+              textAlign: TextAlign.center,
+            ),
+          ].asMap().entries.map((entry) {
+            return SpFadeIn.fromTop(
+              delay: Durations.medium4 + Durations.medium1 * entry.key,
+              duration: Durations.long3,
+              child: entry.value,
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
