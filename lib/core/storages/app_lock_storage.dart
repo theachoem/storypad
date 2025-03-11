cd5b1aa857aa4f24dcad774028c5bdffeb38ba04
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-03-12 00:37:28 +0700

    Close #314 integrate secure storage to store sensitve data

--- lib/core/storages/app_lock_storage.dart
+++ lib/core/storages/app_lock_storage.dart
@@ -1,31 +1,39 @@
+// ignore_for_file: deprecated_member_use_from_same_package
+
 import 'package:storypad/core/objects/app_lock_object.dart';
 import 'package:storypad/core/storages/base_object_storages/object_storage.dart';
 import 'package:storypad/core/storages/base_object_storages/bool_storage.dart';
+import 'package:storypad/core/storages/storage_adapters/base_storage_adapter.dart';
+import 'package:storypad/core/storages/storage_adapters/secure_storage_adaptor.dart';
+import 'package:storypad/core/storages/storage_adapters/share_preferences_storage_adapter.dart';
 
 class AppLockStorage extends ObjectStorage<AppLockObject> {
   @override
-  AppLockObject decode(Map<String, dynamic> json) {
-    return AppLockObject.fromJson(json);
-  }
+  Future<BaseStorageAdapter<String>> get adapter async => SecureStorageAdaptor();
 
   @override
-  Map<String, dynamic> encode(AppLockObject object) {
-    return object.toJson();
-  }
+  AppLockObject decode(Map<String, dynamic> json) => AppLockObject.fromJson(json);
+
+  @override
+  Map<String, dynamic> encode(AppLockObject object) => object.toJson();
 
   @override
   Future<AppLockObject?> readObject() async {
     AppLockObject? data = await super.readObject();
 
-    // TODO: remove this after a while.
-    // ignore: deprecated_member_use_from_same_package
-    bool? deprecatedData = await LocalAuthEnabledStorage().read();
+    bool? deprecatedData1 = await _LocalAuthEnabledStorage().read();
+    AppLockObject? deprecatedData2 = await _AppLockStorage().readObject();
 
-    if (deprecatedData != null) {
-      data = (data ?? AppLockObject.init()).copyWith(enabledBiometric: deprecatedData);
+    if (deprecatedData1 != null) {
+      data = (data ?? AppLockObject.init()).copyWith(enabledBiometric: deprecatedData1);
+      await _LocalAuthEnabledStorage().remove();
+      await writeObject(data);
+    }
 
-      // ignore: deprecated_member_use_from_same_package
-      LocalAuthEnabledStorage().remove();
+    if (deprecatedData2 != null) {
+      data = deprecatedData2;
+      await _AppLockStorage().remove();
+      await writeObject(data);
     }
 
     return data;
@@ -33,4 +41,22 @@ class AppLockStorage extends ObjectStorage<AppLockObject> {
 }
 
 @Deprecated('Will be removed soon.')
-class LocalAuthEnabledStorage extends BoolStorage {}
+class _AppLockStorage extends ObjectStorage<AppLockObject> {
+  @override
+  Future<BaseStorageAdapter<String>> get adapter async => SharePreferencesStorageAdapter();
+
+  @override
+  AppLockObject decode(Map<String, dynamic> json) => AppLockObject.fromJson(json);
+
+  @override
+  Map<String, dynamic> encode(AppLockObject object) => object.toJson();
+
+  @override
+  String get key => 'AppLockStorage';
+}
+
+@Deprecated('Will be removed soon.')
+class _LocalAuthEnabledStorage extends BoolStorage {
+  @override
+  String get key => 'LocalAuthEnabledStorage';
+}
--- lib/core/storages/preference_storages/default_storage.dart
+++ lib/core/storages/preference_storages/default_storage.dart
@@ -1,8 +1,8 @@
 import 'package:storypad/core/storages/preference_storages/base_storage.dart';
 import 'package:storypad/core/storages/storage_adapters/base_storage_adapter.dart';
-import 'package:storypad/core/storages/storage_adapters/default_storage_adapter.dart';
+import 'package:storypad/core/storages/storage_adapters/share_preferences_storage_adapter.dart';
 
 abstract class DefaultStorage<T> extends BaseStorage<T> {
   @override
-  Future<BaseStorageAdapter<T>> get adapter async => DefaultStorageAdapter<T>();
+  Future<BaseStorageAdapter<T>> get adapter async => SharePreferencesStorageAdapter<T>();
 }
