76bb26a69f461d83e62af437c8580288fac2d7df
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-03-15 21:51:05 +0700

    Move body with end drawer to adapt to IOS user

--- lib/views/home/local_widgets/home_scaffold.dart
+++ lib/views/home/local_widgets/home_scaffold.dart
@@ -19,34 +19,52 @@ class _HomeScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      endDrawerEnableOpenDragGesture: true,
-      endDrawer: endDrawer,
-      floatingActionButton: floatingActionButton,
-      bottomNavigationBar: bottomNavigationBar,
-      body: Stack(
-        children: [
-          const StoryListTimelineVerticleDivider(),
-          RefreshIndicator.adaptive(
-            edgeOffset: viewModel.scrollInfo.appBar(context).getExpandedHeight() + MediaQuery.of(context).padding.top,
-            onRefresh: () => viewModel.refresh(context),
-            child: CustomScrollView(
-              controller: viewModel.scrollInfo.scrollController,
-              physics: const AlwaysScrollableScrollPhysics(),
-              slivers: [
-                appBar,
-                body,
+    return SpSingleStateWidget(
+      initialValue: false,
+      builder: (context, endDrawerOpenedNotifier) {
+        return Scaffold(
+          endDrawerEnableOpenDragGesture: true,
+          endDrawer: endDrawer,
+          floatingActionButton: floatingActionButton,
+          bottomNavigationBar: bottomNavigationBar,
+          onEndDrawerChanged: AppTheme.isIOS(context) ? (isOpened) => endDrawerOpenedNotifier.value = isOpened : null,
+          body: ValueListenableBuilder(
+            valueListenable: endDrawerOpenedNotifier,
+            child: Stack(
+              children: [
+                const StoryListTimelineVerticleDivider(),
+                RefreshIndicator.adaptive(
+                  edgeOffset:
+                      viewModel.scrollInfo.appBar(context).getExpandedHeight() + MediaQuery.of(context).padding.top,
+                  onRefresh: () => viewModel.refresh(context),
+                  child: CustomScrollView(
+                    controller: viewModel.scrollInfo.scrollController,
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    slivers: [
+                      appBar,
+                      body,
+                    ],
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: MediaQuery.of(context).padding.bottom + 12.0,
+                  child: _AppUpdateFloatingButton(),
+                ),
               ],
             ),
+            builder: (context, endDrawerOpened, child) {
+              return AnimatedContainer(
+                transform: Matrix4.identity()..translate(endDrawerOpened ? -304.0 / 2 : 0.0, 0.0),
+                duration: Durations.long1,
+                curve: Curves.ease,
+                child: child,
+              );
+            },
           ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: MediaQuery.of(context).padding.bottom + 12.0,
-            child: _AppUpdateFloatingButton(),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
