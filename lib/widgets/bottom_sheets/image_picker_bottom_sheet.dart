abab14eb9c1e77a8098bf948aa27bac6acdbf742
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-03-15 20:47:04 +0700

    Move image picker service to sheet instead

--- lib/widgets/bottom_sheets/image_picker_bottom_sheet.dart
+++ lib/widgets/bottom_sheets/image_picker_bottom_sheet.dart
@@ -3,44 +3,30 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:path/path.dart' as path;
 import 'package:storypad/core/constants/app_constants.dart';
 import 'package:storypad/core/databases/models/asset_db_model.dart';
 import 'package:storypad/core/services/analytics/analytics_service.dart';
+import 'package:storypad/widgets/bottom_sheets/base_bottom_sheet.dart';
 import 'package:storypad/widgets/custom_embed/sp_image.dart';
+import 'package:path/path.dart' as path;
 
-class ImagePickerService {
-  final QuillController controller;
-
-  ImagePickerService({
-    required this.controller,
+class ImagePickerBottomSheet extends BaseBottomSheet {
+  const ImagePickerBottomSheet({
+    required this.assets,
   });
 
-  Future<void> showSheet(BuildContext context) async {
+  final List<AssetDbModel> assets;
+
+  static Future<void> showPicker<T>({
+    required BuildContext context,
+    required QuillController controller,
+  }) async {
     final assets = await AssetDbModel.db.where().then((e) => e?.items ?? <AssetDbModel>[]);
-
     if (!context.mounted) return;
-    double statusBarHeight = MediaQuery.of(context).padding.top;
 
-    final asset = await showModalBottomSheet(
-      isScrollControlled: true,
-      showDragHandle: true,
-      context: context,
-      builder: (context) {
-        double height = MediaQuery.of(context).size.height;
-
-        return DraggableScrollableSheet(
-          expand: false,
-          maxChildSize: (height - statusBarHeight) / height,
-          builder: (context, controller) {
-            return _PhotoSheet(
-              assets: assets,
-              scrollController: controller,
-            );
-          },
-        );
-      },
-    );
+    final asset = await ImagePickerBottomSheet(
+      assets: assets,
+    ).show(context: context);
 
     if (asset is AssetDbModel) {
       final index = controller.selection.baseOffset;
@@ -50,20 +36,8 @@ class ImagePickerService {
       controller.moveCursorToPosition(index + 1);
     }
   }
-}
 
-// TODO: extract this
-
-class _PhotoSheet extends StatelessWidget {
-  const _PhotoSheet({
-    required this.assets,
-    required this.scrollController,
-  });
-
-  final List<AssetDbModel> assets;
-  final ScrollController scrollController;
-
-  Future<void> insertFromPhotoLibrary(BuildContext context) async {
+  Future<void> _insertFromPhotoLibrary(BuildContext context) async {
     final result = await FilePicker.platform.pickFiles(type: FileType.image);
 
     if (result?.files.isNotEmpty == true) {
@@ -99,40 +73,57 @@ class _PhotoSheet extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        appBar: AppBar(title: Text("$kAppName Library")),
-        body: buildBody(context, constraints),
-        bottomNavigationBar: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Divider(height: 1),
-            Container(
-              padding: EdgeInsets.only(
-                left: 8.0,
-                top: 8.0,
-                bottom: MediaQuery.of(context).padding.bottom + 8.0,
-                right: 16.0,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  OutlinedButton.icon(
-                    icon: Icon(Icons.add_a_photo),
-                    label: Text("Insert from Device"),
-                    onPressed: () => insertFromPhotoLibrary(context),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      );
-    });
+  Widget build(BuildContext context, double bottomPadding) {
+    return DraggableScrollableSheet(
+      expand: false,
+      builder: (context, scrollController) {
+        return LayoutBuilder(builder: (context, constraints) {
+          return buildScaffold(context, constraints, scrollController);
+        });
+      },
+    );
   }
 
-  Widget buildBody(BuildContext context, BoxConstraints constraints) {
+  Widget buildScaffold(BuildContext context, BoxConstraints constraints, ScrollController scrollController) {
+    return Scaffold(
+      appBar: AppBar(title: Text("$kAppName Library")),
+      body: buildBody(
+        context: context,
+        constraints: constraints,
+        scrollController: scrollController,
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Divider(height: 1),
+          Container(
+            padding: EdgeInsets.only(
+              left: 8.0,
+              top: 8.0,
+              bottom: MediaQuery.of(context).padding.bottom + 8.0,
+              right: 16.0,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                OutlinedButton.icon(
+                  icon: Icon(Icons.add_a_photo),
+                  label: Text("Insert from Device"),
+                  onPressed: () => _insertFromPhotoLibrary(context),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildBody({
+    required BuildContext context,
+    required BoxConstraints constraints,
+    required ScrollController scrollController,
+  }) {
     if (assets.isEmpty) {
       return Center(
         child: Text(
