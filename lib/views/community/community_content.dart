3eff37dc2cd622f3eabde66039143a91f016b62c
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-05-10 19:19:47 +0700

    Fix: hide tile if URL is empty

--- lib/views/community/community_content.dart
+++ lib/views/community/community_content.dart
@@ -21,22 +21,25 @@ class _CommunityContent extends StatelessWidget {
           const SizedBox(height: 8.0),
           const CommunityCard(),
           const SizedBox(height: 12.0),
-          ListTile(
-            leading: const Icon(SpIcons.question),
-            title: Text(tr("list_tile.faq.title")),
-            onTap: () => UrlOpenerService.openInCustomTab(context, RemoteConfigService.faqUrl.get()),
-          ),
-          ListTile(
-            leading: const Icon(SpIcons.policy),
-            title: Text(tr("list_tile.privacy_policy.title")),
-            onTap: () => UrlOpenerService.openInCustomTab(context, RemoteConfigService.policyPrivacyUrl.get()),
-          ),
-          ListTile(
-            leading: const Icon(SpIcons.code),
-            title: Text(tr("list_tile.source_code.title")),
-            subtitle: Text(tr("list_tile.source_code.subtitle")),
-            onTap: () => UrlOpenerService.openInCustomTab(context, RemoteConfigService.sourceCodeUrl.get()),
-          ),
+          if (RemoteConfigService.faqUrl.get().trim().isNotEmpty == true)
+            ListTile(
+              leading: const Icon(SpIcons.question),
+              title: Text(tr("list_tile.faq.title")),
+              onTap: () => UrlOpenerService.openInCustomTab(context, RemoteConfigService.faqUrl.get()),
+            ),
+          if (RemoteConfigService.policyPrivacyUrl.get().trim().isNotEmpty == true)
+            ListTile(
+              leading: const Icon(SpIcons.policy),
+              title: Text(tr("list_tile.privacy_policy.title")),
+              onTap: () => UrlOpenerService.openInCustomTab(context, RemoteConfigService.policyPrivacyUrl.get()),
+            ),
+          if (RemoteConfigService.sourceCodeUrl.get().trim().isNotEmpty == true)
+            ListTile(
+              leading: const Icon(SpIcons.code),
+              title: Text(tr("list_tile.source_code.title")),
+              subtitle: Text(tr("list_tile.source_code.subtitle")),
+              onTap: () => UrlOpenerService.openInCustomTab(context, RemoteConfigService.sourceCodeUrl.get()),
+            ),
           const Divider(),
           ListTile(
             leading: const Icon(SpIcons.onboarding),
