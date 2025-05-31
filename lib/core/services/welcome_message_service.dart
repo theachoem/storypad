a08c24fcf785d84a08fef16fc422fa78a7258f92
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-05-31 13:42:52 +0700

    Only display random random message for english

--- lib/core/services/welcome_message_service.dart
+++ lib/core/services/welcome_message_service.dart
@@ -2,41 +2,44 @@ import 'dart:math';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
+// Only return random message for english, other language translation are not match the the phrase & make app feel unfinished.
 class WelcomeMessageService {
   static int? _index;
 
-  static String get(BuildContext? context) {
+  static String get(BuildContext context) {
+    if (context.locale.languageCode != 'en') return tr('page.home.app_bar.messages.what_in_ur_mind');
+
     final messages = {
-      tr("page.home.app_bar.messages.what_in_ur_mind", context: context),
-      tr("page.home.app_bar.messages.your_story", context: context),
-      tr("page.home.app_bar.messages.new_page", context: context),
-      tr("page.home.app_bar.messages.let_it_flow", context: context),
-      tr("page.home.app_bar.messages.capture_moment", context: context),
-      tr("page.home.app_bar.messages.adventure_awaits", context: context),
-      tr("page.home.app_bar.messages.thoughts_lead", context: context),
-      tr("page.home.app_bar.messages.dream_it", context: context),
-      tr("page.home.app_bar.messages.start_word", context: context),
-      tr("page.home.app_bar.messages.exciting_today", context: context),
-      tr("page.home.app_bar.messages.story_here", context: context),
-      tr("page.home.app_bar.messages.write_freely", context: context),
-      tr("page.home.app_bar.messages.seed_grow", context: context),
-      tr("page.home.app_bar.messages.feelings_now", context: context),
-      tr("page.home.app_bar.messages.jot_it_down", context: context),
-      tr("page.home.app_bar.messages.fresh_page", context: context),
-      tr("page.home.app_bar.messages.memory_today", context: context),
-      tr("page.home.app_bar.messages.your_rules", context: context),
-      tr("page.home.app_bar.messages.big_ideas", context: context),
-      tr("page.home.app_bar.messages.wander_free", context: context),
-      tr("page.home.app_bar.messages.raw_real", context: context),
-      tr("page.home.app_bar.messages.today_memory", context: context),
-      tr("page.home.app_bar.messages.future_self", context: context),
-      tr("page.home.app_bar.messages.just_write", context: context),
-      tr("page.home.app_bar.messages.highlight_today", context: context),
-      tr("page.home.app_bar.messages.your_space", context: context),
-      tr("page.home.app_bar.messages.magic_words", context: context),
-      tr("page.home.app_bar.messages.feelings_flow", context: context),
-      tr("page.home.app_bar.messages.say_something", context: context),
-      tr("page.home.app_bar.messages.page_waiting", context: context),
+      "Let your adventure unfold with every word you write.",
+      "Your story unfolds one thought at a time.",
+      "Capture this moment.",
+      "Dream it. Write it.",
+      "Anything exciting today?",
+      "Are today's feelings worth writing down?",
+      "What feelings move you today?",
+      "What's on the next page of your journey?",
+      "What would you tell your future self?",
+      "What's today's highlight?",
+      "Jot it down before it fades.",
+      "No pressure, just write.",
+      "Let your words flow.",
+      "Make everyday moments extraordinary.",
+      "What's a moment you'll never forget?",
+      "A new page, a new beginning.",
+      "The page is open, waiting for you.",
+      "Express yourself truthfully.",
+      "What's on your heart?",
+      "Allow your thoughts to grow.",
+      "Start with a word.",
+      "Your story continues here.",
+      "Let your thoughts lead.",
+      "Today's thoughts, tomorrow's memories.",
+      "Let your mind wander.",
+      "What's on your mind?",
+      "Here, your words are safe.",
+      "Your diary, your rules.",
+      "This is your space, make it yours.",
+      "Tell your story, no one else can.",
     };
 
     _index ??= Random().nextInt(messages.length);
