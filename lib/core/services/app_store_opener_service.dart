82002a8a30641efe87006c4602699dee5c362068
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-05-31 14:05:16 +0700

    Fix app store not opening

--- lib/core/services/app_store_opener_service.dart
+++ lib/core/services/app_store_opener_service.dart
@@ -5,20 +5,17 @@ import 'package:storypad/core/constants/app_constants.dart';
 import 'package:storypad/core/services/url_opener_service.dart';
 
 class AppStoreOpenerService {
-  static Future<void> call({
-    String? packageName,
-    String? appStoreId,
-  }) async {
+  static Future<void> call() async {
     final InAppReview inAppReview = InAppReview.instance;
 
     Future<void> openStore() async {
       if (Platform.isAndroid) {
-        String deeplink = 'market://details?id=${packageName ?? kPackageInfo.packageName}';
+        String deeplink = 'market://details?id=${kPackageInfo.packageName}';
         bool launched = await UrlOpenerService.launchUrlString(deeplink);
         if (launched) return;
       }
 
-      await inAppReview.openStoreListing(appStoreId: appStoreId);
+      await inAppReview.openStoreListing(appStoreId: '6744032172');
     }
 
     await openStore();
