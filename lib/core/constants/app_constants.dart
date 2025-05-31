7d5f1b6f8ab8220b8131cc14e49b432dc680922e
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-06-01 02:10:55 +0700

    Bump version to 2.11.6+366 for official release

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-// import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -23,9 +23,8 @@ final bool kSpooky = kPackageInfo.packageName == 'com.juniorise.spooky';
 final bool kStoryPad = kPackageInfo.packageName == 'com.tc.writestory';
 final bool kCommunity = kPackageInfo.packageName == 'com.juniorise.spooky.community';
 
-// TODO: Disable for now.
-// const bool kHasRelaxSoundsFeature = false || kDebugMode;
-const bool kHasRelaxSoundsFeature = true;
+// Disable for now.
+const bool kHasRelaxSoundsFeature = false || kDebugMode;
 
 late final Directory kSupportDirectory;
 late final Directory kApplicationDirectory;
