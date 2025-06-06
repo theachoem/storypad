fedcb4b9c1679102a27c65c30bbce7534c4e7e89
LengTech11 <soklenghoungofficial11@gmail.com>
2025-06-06 11:11:22 +0700

    Close #402 require security to access archives or bins

--- lib/views/archives/archives_view.dart
+++ lib/views/archives/archives_view.dart
@@ -4,6 +4,7 @@ import 'package:storypad/widgets/sp_icons.dart';
 import 'package:storypad/widgets/sp_multi_edit_bottom_nav_bar.dart';
 import 'package:storypad/widgets/base_view/view_model_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:storypad/core/objects/search_filter_object.dart';
 import 'package:storypad/core/types/path_type.dart';
 import 'package:storypad/widgets/base_view/base_route.dart';
@@ -11,12 +12,28 @@ import 'package:storypad/widgets/sp_fade_in.dart';
 import 'package:storypad/widgets/sp_pop_up_menu_button.dart';
 import 'package:storypad/widgets/story_list/sp_story_list.dart';
 import 'package:storypad/widgets/story_list/sp_story_list_multi_edit_wrapper.dart';
+import 'package:storypad/providers/app_lock_provider.dart';
 
 import 'archives_view_model.dart';
 
 part 'archives_content.dart';
 
 class ArchivesRoute extends BaseRoute {
+  @override
+  Future<T?> push<T extends Object?>(
+    BuildContext context, {
+    bool rootNavigator = false,
+  }) async {
+    bool authenticated =
+        await context.read<AppLockProvider>().authenticateIfHas(context: context, debugSource: '$runtimeType#push');
+    if (!authenticated || !context.mounted) return null;
+
+    return super.push(
+      context,
+      rootNavigator: rootNavigator,
+    );
+  }
+
   @override
   Widget buildPage(BuildContext context) => ArchivesView(params: this);
 }
