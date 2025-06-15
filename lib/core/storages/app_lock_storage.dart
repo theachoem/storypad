226c0d3a4bcba524a9d6df5af440bf1474208522
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-06-15 12:34:15 +0700

    Remove deprecate lock storage

--- lib/core/storages/app_lock_storage.dart
+++ lib/core/storages/app_lock_storage.dart
@@ -2,10 +2,8 @@
 
 import 'package:storypad/core/objects/app_lock_object.dart';
 import 'package:storypad/core/storages/base_object_storages/object_storage.dart';
-import 'package:storypad/core/storages/base_object_storages/bool_storage.dart';
 import 'package:storypad/core/storages/storage_adapters/base_storage_adapter.dart';
 import 'package:storypad/core/storages/storage_adapters/secure_storage_adaptor.dart';
-import 'package:storypad/core/storages/storage_adapters/share_preferences_storage_adapter.dart';
 
 class AppLockStorage extends ObjectStorage<AppLockObject> {
   @override
@@ -16,47 +14,4 @@ class AppLockStorage extends ObjectStorage<AppLockObject> {
 
   @override
   Map<String, dynamic> encode(AppLockObject object) => object.toJson();
-
-  @override
-  Future<AppLockObject?> readObject() async {
-    AppLockObject? data = await super.readObject();
-
-    bool? deprecatedData1 = await _LocalAuthEnabledStorage().read();
-    AppLockObject? deprecatedData2 = await _AppLockStorage().readObject();
-
-    if (deprecatedData1 != null) {
-      data = (data ?? AppLockObject.init()).copyWith(enabledBiometric: deprecatedData1);
-      await _LocalAuthEnabledStorage().remove();
-      await writeObject(data);
-    }
-
-    if (deprecatedData2 != null) {
-      data = deprecatedData2;
-      await _AppLockStorage().remove();
-      await writeObject(data);
-    }
-
-    return data;
-  }
-}
-
-@Deprecated('Will be removed soon.')
-class _AppLockStorage extends ObjectStorage<AppLockObject> {
-  @override
-  Future<BaseStorageAdapter<String>> get adapter async => SharePreferencesStorageAdapter();
-
-  @override
-  AppLockObject decode(Map<String, dynamic> json) => AppLockObject.fromJson(json);
-
-  @override
-  Map<String, dynamic> encode(AppLockObject object) => object.toJson();
-
-  @override
-  String get key => 'AppLockStorage';
-}
-
-@Deprecated('Will be removed soon.')
-class _LocalAuthEnabledStorage extends BoolStorage {
-  @override
-  String get key => 'LocalAuthEnabledStorage';
 }
