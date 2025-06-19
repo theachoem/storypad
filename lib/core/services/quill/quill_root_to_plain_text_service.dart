b408c2c5d134474e7ef7742ff5dc61834ef4aa77
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-06-20 00:23:14 +0700

    Improve to plain text to work with ordered list

--- lib/core/services/quill/quill_root_to_plain_text_service.dart
+++ lib/core/services/quill/quill_root_to_plain_text_service.dart
@@ -15,17 +15,17 @@ class QuillRootToPlainTextService {
     LinkedList<Node> children,
     bool markdown, {
     String prefix = '',
+    Map<int, int>? orderedListCounter,
   }) {
-    return children.map((node) {
-      if (node.toPlainText().contains('storypad')) {
-        // print(node.runtimeType);
-      }
+    orderedListCounter ??= {};
 
+    return children.map((node) {
       if (node is Block) {
-        return extract(node.children, markdown, prefix: prefix);
+        return extract(node.children, markdown, prefix: prefix, orderedListCounter: orderedListCounter);
       } else if (node is Line) {
         final attrs = node.style.attributes;
-        final indent = '\t' * (attrs['indent']?.value ?? 0);
+        final indentLevel = int.tryParse("${attrs['indent']?.value}");
+        final indent = '\t' * (indentLevel ?? 0);
         final list = attrs['list']?.value;
         final isBlockquote = attrs.containsKey('blockquote');
         final isCodeBlock = attrs.containsKey('code-block');
@@ -33,16 +33,14 @@ class QuillRootToPlainTextService {
         String linePrefix = indent;
 
         if (isCodeBlock) {
-          final content = extract(node.children, markdown, prefix: linePrefix);
+          final content = extract(node.children, markdown, prefix: linePrefix, orderedListCounter: orderedListCounter);
           return '```\n$content\n```\n';
         }
 
         // output: >> for multiple indent.
         if (isBlockquote) {
-          final content = extract(node.children, markdown, prefix: '');
-
-          final indentCount = attrs['indent']?.value;
-          final quotePrefix = indentCount != null ? '> ' * (indentCount + 1) : '> ';
+          final content = extract(node.children, markdown, prefix: '', orderedListCounter: orderedListCounter);
+          final quotePrefix = indentLevel != null ? '> ' * (indentLevel + 1) : '> ';
 
           return '$quotePrefix$content\n';
         }
@@ -50,7 +48,15 @@ class QuillRootToPlainTextService {
         if (list == 'bullet') {
           linePrefix += '- ';
         } else if (list == 'ordered') {
-          linePrefix += '1. ';
+          int index = orderedListCounter![indentLevel ?? 0] = (orderedListCounter[indentLevel ?? 0] ?? 0) + 1;
+          final formattedIndex = switch (indentLevel) {
+            0 => '$index.',
+            1 => '${String.fromCharCode(96 + index)}.',
+            2 => '${_toRoman(index).toLowerCase()}.',
+            _ => '$index.',
+          };
+
+          linePrefix += '$formattedIndex ';
         } else if (list == 'checked') {
           linePrefix += '- [x] ';
         } else if (list == 'unchecked') {
@@ -94,5 +100,35 @@ class QuillRootToPlainTextService {
         return node.toPlainText();
       }
     }).join();
+  }
+
+  static String _toRoman(int number) {
+    final numerals = {
+      1000: 'M',
+      900: 'CM',
+      500: 'D',
+      400: 'CD',
+      100: 'C',
+      90: 'XC',
+      50: 'L',
+      40: 'XL',
+      10: 'X',
+      9: 'IX',
+      5: 'V',
+      4: 'IV',
+      1: 'I',
+    };
+
+    var result = '';
+    var n = number;
+
+    numerals.forEach((value, symbol) {
+      while (n >= value) {
+        result += symbol;
+        n -= value;
+      }
+    });
+
+    return result;
   }
 }
