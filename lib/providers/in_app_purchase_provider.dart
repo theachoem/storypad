fa9d23c37a10508f3ebb651f3283beff9042ff9b
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-08-10 15:39:23 +0700

    Close #440 fix unable to establish connection on channel

--- lib/providers/in_app_purchase_provider.dart
+++ lib/providers/in_app_purchase_provider.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
