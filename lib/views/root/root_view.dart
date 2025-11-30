b7043d771aa7fdcb5c166d12f948501449b8d0fc
Thea Choem <theacheng.g6@gmail.com>
2025-12-01 01:10:09 +0700

    Add relaxing sound button

--- lib/views/root/root_view.dart
+++ lib/views/root/root_view.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:storypad/core/storages/new_badge_storage.dart';
 import 'package:storypad/providers/app_lock_provider.dart';
 import 'package:storypad/views/library/library_view.dart';
+import 'package:storypad/views/relax_sounds/relax_sounds_view.dart';
 import 'package:storypad/widgets/base_view/view_model_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
