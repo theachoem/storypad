4b8e8410dc344b078222baa8366b0dd5ca4e441f
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-12-07 04:31:15 +0700

    Add enter to edit story & ctr+s to save story kbd shortcut (#537)

--- lib/views/stories/edit/edit_story_view_model.dart
+++ lib/views/stories/edit/edit_story_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:storypad/core/databases/models/story_content_db_model.dart';
 import 'package:storypad/core/databases/models/story_db_model.dart';
@@ -153,6 +154,22 @@ class EditStoryViewModel extends BaseStoryViewModel {
         requested = true;
       }
     });
+  }
+
+  void handleKeyEvent(KeyEvent event, BuildContext context) {
+    if (event is KeyDownEvent) {
+      final isCtrlOrCmd =
+          HardwareKeyboard.instance.isLogicalKeyPressed(LogicalKeyboardKey.controlLeft) ||
+          HardwareKeyboard.instance.isLogicalKeyPressed(LogicalKeyboardKey.controlRight) ||
+          HardwareKeyboard.instance.isLogicalKeyPressed(LogicalKeyboardKey.metaLeft) ||
+          HardwareKeyboard.instance.isLogicalKeyPressed(LogicalKeyboardKey.metaRight);
+
+      final isS = event.logicalKey == LogicalKeyboardKey.keyS;
+
+      if (isCtrlOrCmd && isS) {
+        done(context);
+      }
+    }
   }
 
   Future<void> onPopInvokedWithResult(bool didPop, Object? _, BuildContext context) async {
