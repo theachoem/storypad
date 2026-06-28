1d1bb4387f118a6d2a99453a0dc4b7126fe7c57d
Thea Choem <theacheng.g6@gmail.com>
2026-06-28 12:21:03 +0700

    Move Import/Export section into setting directly for better UX

--- lib/views/backup_services/backup_services_content.dart
+++ lib/views/backup_services/backup_services_content.dart
@@ -28,12 +28,6 @@ class _BackupServicesContent extends StatelessWidget {
         children: [
           const SizedBox(height: 8.0),
           ..._buildCloudServiceTiles(context),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.folder_open_outlined),
-            title: Text(tr('page.import_export_backup')),
-            onTap: () => const ImportExportRoute().push(context),
-          ),
         ],
       ),
     );
